ad7f6bd3051d6a875cab6eb44c318ccc3d402f11
tussupov0209 <tussupov0209@gmail.com>
2023-06-18 21:41:26 +0600

    numb8

---
+++ numb8/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const fs = require('fs/promises')
+const connections = [];
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  connections.push(socket.id);
+  io.emit('connections', connections);
+  socket.on('message', (message) => {
+    console.log(`Сообщение от ${socket.id}: ${message}`);
+    io.emit('message', message)
+  });
+  socket.on('disconnect', () => {
+    const index = connections.indexOf(socket.id);
+    if (index !== -1) {
+      connections.splice(index, 1);
+    }
+
+    io.emit('connections', connections);
+  });
+});
+
+server.listen(8000, () => {
+  console.log('Сервер запущен на порту 8000');
+});
